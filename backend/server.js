be68ae9c6b2aedcef537b4e32452beae89b95794
01fe22bca209 <156529557+01fe22bca209@users.noreply.github.com>
2024-12-17 01:41:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 // Middleware
 app.use(cors(
   {
-    origin:["https://kamaxi-mern-frontend.vercel.app/"],
+    origin:["https://kamaxi2-mern-frontend.vercel.app/"],
     methods:["POST","GET"],
     credentials:true}
 )); // Enable CORS
