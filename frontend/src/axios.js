300a80a940225a59286be49cf6a87a73d5f37cf3
01fe22bca209 <156529557+01fe22bca209@users.noreply.github.com>
2024-12-17 10:50:59 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/users",
+    baseURL: "https://kamaxi3-updated-api.vercel.app/users",
 });
 
 export default instance;
